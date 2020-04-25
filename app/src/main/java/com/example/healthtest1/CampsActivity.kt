5cdd10587cbb8afc40a6a40c40059b4523d17782
Kevin Paul <kevinpaul1100@gmail.com>
2020-04-25 22:03:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/healthtest1/CampsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healthtest1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CampsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camps)
+    }
+}
---
+++ app/src/main/java/com/example/healthtest1/HospitalActivity.kt
@@ -0,0 +1,14 @@
+package com.example.healthtest1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HospitalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hospital)
+
+
+    }
+}
